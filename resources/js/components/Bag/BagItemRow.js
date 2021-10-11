c915279f3765bf5e26df040601175742d0ed7e41
TomF46 <t-d-f@live.co.uk>
2021-10-11 16:17:56 +0100

    Correct path for URL in bag row

--- resources/js/components/Bag/BagItemRow.js
+++ resources/js/components/Bag/BagItemRow.js
@@ -12,7 +12,7 @@ const BagItemRow = ({ product }) => {
     return (
         <div className="col-span-12 rounded overflow-hidden shadow-lg pointer mx-4 mb-2">
             <div className="grid grid-cols-12">
-                <div className="col-span-2 h-32 bg-cover  hover:bg-gray" style={{ backgroundImage: `url(${product.images[0].url})` }}></div>
+                <div className="col-span-2 h-32 bg-cover  hover:bg-gray" style={{ backgroundImage: `url(${product.images[0]})` }}></div>
                 <div className="cold-span-10 p-4">
                     <div className="font-bold text-xl">{product.name}</div>
                     <div className="font-bold text-gray"><MoneyFormat value={product.price} /></div>
